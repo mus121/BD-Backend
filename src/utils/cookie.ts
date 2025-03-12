c5734462aa2dcb4efe6492ade3aa076a9d0374e0
mus121 <daniyalkamal56@gmail.com>
2025-03-12 15:48:12 +0500

    Push Cookies

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -5,32 +5,32 @@ export const setCookies = (req: Request, res: Response) => {
     const { accessToken, refreshToken, userInfo } = res.locals.authData;
 
     res.cookie('access_token', accessToken, {
-      httpOnly: true,
-      secure: true,
+      httpOnly: false,
+      secure: false,
       sameSite: 'none',
       path: '/',
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie('refresh_token', refreshToken, {
-      httpOnly: true,
-      secure: true,
+      httpOnly: false,
+      secure: false,
       sameSite: 'none',
       path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.cookie('user_id', userInfo.id, {
-      httpOnly: true,
-      secure: true,
+      httpOnly: false,
+      secure: false,
       sameSite: 'none',
       path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.cookie('user_email', userInfo.email, {
-      httpOnly: true,
-      secure: true,
+      httpOnly: false,
+      secure: false,
       sameSite: 'none',
       path: '/',
       maxAge: 7 * 24 * 60 * 60 * 1000,
